7b87f1c3673c804afcd8bbd81910ebfe0f91c0aa
Barath krishnan <barathkrishnan515@gmail.com>
2025-05-27 11:02:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB connection
